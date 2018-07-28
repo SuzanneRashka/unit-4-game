de97b2c9942f2f39f2fed6ffeb2c782d96319b1a
SuzanneRashka <suzanne.rashka@gmail.com>
2018-07-27 23:35:57 -0500

    file stuctures/visual added

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// #choice-character-name / char name
+// #choice-health-points / characters health points
+// #opponent-character-name / char name
+// #opponent-health-points / characters health points
+$(document).ready(function() {});
